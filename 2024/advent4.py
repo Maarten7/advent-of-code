5f4c354b03fbbda85391cedfb984b6661e7abd01
Maarten Post <h.m.m.post@hva.nl>
2024-12-04 19:14:16 +0100

    day4

--- 2024/advent4.py
+++ 2024/advent4.py
@@ -1,27 +1,67 @@
 import numpy as np
 import os
+import re
 from pathlib import Path
 
+def count_xmas(letters: str):
+    counts = re.findall("XMAS", letters)
+    return len(counts)
 
-def main_1(array):
-    pass
+def main_1(file):
+    total = 0
+    input = np.loadtxt(file, dtype=str)
+    input = np.array([list(string) for string in input])
 
-def main_2(array):
-    pass
+    total += sum([count_xmas("".join(inpu)) for inpu in input])
+    total += sum([count_xmas("".join(inpu)) for inpu in input[:, ::-1]])
+    total += sum([count_xmas("".join(inpu)) for inpu in input.T])
+    total += sum([count_xmas("".join(inpu)) for inpu in input.T[:, ::-1]])
 
+    for offset in range(-input.shape[0], input.shape[0] ):
+        total += count_xmas("".join(np.diagonal(input, offset=offset)))
+        total += count_xmas("".join(np.diagonal(input, offset=offset))[::-1])
+        total += count_xmas("".join(np.diagonal(input[:, ::-1], offset=offset)))
+        total += count_xmas("".join(np.diagonal(input[:, ::-1], offset=offset))[::-1])
+
+    return total
+
+
+def main_2(file):
+    total = 0
+    input = np.loadtxt(file, dtype=str)
+    input = np.array([list(string) for string in input])
+
+    letters = set(["M", "S"])
+
+    for i, j in np.dstack(np.where(input == 'A')).reshape(-1, 2):
+
+        if i in [0, input.shape[0] - 1] or j in [0, input.shape[1] - 1]:
+            continue
+
+        lu = input[i-1, j-1]
+        ld = input[i-1, j+1]
+        ru = input[i+1, j-1]
+        rd = input[i+1, j+1]
+
+
+        if set([lu, rd]) == letters and set([ld, ru]) == letters:
+            total += 1
+
+    return total
 
 if __name__ == "__main__":
     advent_day = Path(os.path.basename(__file__)).stem
 
     file = f"test_input_{advent_day}.txt"
-    input = np.loadtxt(file, dtype=int)
 
-    test_1 = main_1(input); print(test_1)
-    if test_1 == 0:
-        input = np.loadtxt(f"input_{advent_day}.txt", dtype=int)
-        print(main_1(input))
+    test_1 = main_1(file)
+    print(test_1)
+    if test_1 == 18:
+        file = f"input_{advent_day}.txt"
+        print(main_1(file))
 
-    test_2 = main_2(input)
-    if test_2 == 0:
-        input = np.loadtxt(f"input_{advent_day}.txt", dtype=int)
-        print(main_2(input))
+    test_2 = main_2(file)
+    print(test_2)
+    if test_2 == 9:
+        file = f"input_{advent_day}.txt"
+        print(main_2(file))
