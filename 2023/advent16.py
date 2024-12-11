10f9df162b2c8efe1f37e500ec2f7c596d02c4bb
Maarten Post <h.m.m.post@hva.nl>
2024-12-11 16:42:38 +0100

    finally!!!

--- 2023/advent16.py
+++ 2023/advent16.py
@@ -1,107 +1,87 @@
 import numpy as np
 
-grid = []
-history = []
-for line in open("advent161.txt"):
-    grid.append(list(line.strip()))
+DOWN = (1, 0)
+UP = (-1, 0)
+RIGHT = (0, 1)
+LEFT = (0, -1)
+directions = [UP, DOWN, RIGHT, LEFT]
+direction_str = {UP: "^", DOWN: "v", RIGHT: ">", LEFT: "<"}
+forward_mirror = {UP: RIGHT, DOWN: LEFT, RIGHT: UP, LEFT: DOWN}
+backward_mirror = {UP: LEFT, DOWN: RIGHT, RIGHT: DOWN, LEFT: UP}
 
-grid = np.array(grid)
-history = [[set() for j in range(len(grid[0]))] for i in range(len(grid))]
-history = np.array(history)
-draw = np.full(shape=grid.shape, fill_value=".")
-energy = np.zeros(shape=grid.shape)
-energy_draw = np.full(shape=grid.shape, fill_value='.')
+grid = np.array([list(line.strip()) for line in open("advent16.txt")])
+max_i, max_j = grid.shape
 
 
-def update_position(i, j, direction):
-    if direction == ">":
-        if j + 1 >= len(grid[0]):
+def re_run_light(pos, dir):
+    history = set()
+    energy = np.zeros(shape=grid.shape)
+
+
+    def update_position(position, direction):
+        i, j = position
+        di, dj = direction
+        new_position = i + di, j + dj
+        return new_position
+
+
+    def update_direction(position, direction):
+        item = grid[position]
+
+        if item == "\\":
+            return backward_mirror[direction]
+        if item == "/":
+            return forward_mirror[direction]
+
+        if (direction, item) in [
+            (UP, "-"),
+            (DOWN, "-"),
+        ]:
+            run_light(position, LEFT)
+            run_light(position, RIGHT)
             return
-        return i, j + 1
-    if direction == "<":
-        if j -1 < 0:
+
+        if (direction, item) in [
+            (RIGHT, "|"),
+            (LEFT, "|"),
+        ]:
+            run_light(position, UP)
+            run_light(position, DOWN)
             return
-        return i, j - 1
-    if direction == "^":
-        if i - 1 < 0:
-            return
-        return i - 1, j
-    if direction == "v":
-        if i + 1 >= len(grid):
-            return
-        return i + 1, j
 
-
-def update_direction(position, direction):
-    item = grid[position]
-
-    energy[position] = 1
-    if item == ".":
-        draw[position] = direction
-        return direction
-    if item == "\\":
-        mirror = {">": "v", "<": "^", "^": "<", "v": ">"}
-        return mirror[direction]
-    if item == "/":
-        mirror = {">": "^", "<": "v", "^": ">", "v": "<"}
-        return mirror[direction]
-
-    if (direction, item) in [
-        (">", "-"),
-        ("<", "-"),
-        ("^", "|"),
-        ("v", "|"),
-    ]:
         return direction
 
-    if (direction, item) in [
-        ("^", "-"),
-        ("v", "-"),
-    ]:
-        run_light(position, "<")
-        run_light(position, ">")
-        return
-    if (direction, item) in [
-        (">", "|"),
-        ("<", "|"),
-    ]:
-        run_light(position, "^")
-        run_light(position, "v")
-        return
 
-def run_light(position, direction):
-    i, j = position
-    energy[position] = 1
-    energy_draw[position] = '#'
-    if i < 0 or j < 0:
-        return
-    if i >= len(grid) or j >= len(grid[0]):
-        return
-
-    while 1:
-        draw[position] = direction 
-        print(draw)
-        if direction in history[position]:
-            return
-        history[position].add(direction)
-
-        position = update_position(*position, direction=direction)
-        if not position:
-            break
-
-        direction = update_direction(position, direction)
-        if not direction:
-            return
-
-        energy[position] = 1
-        energy_draw[position] = '#'
+    def in_bound(position):
+        i, j = position
+        return 0 <= i < max_i and 0 <= j < max_j
 
 
-print(grid)
-print()
+    def run_light(position, direction):
 
-run_light((0, 0), ">")
-print(draw)
-print()
-print(energy_draw)
-print(int(energy.sum()))
+        while True:
+            if (position, direction) in history:
+                return
+
+            history.add((position, direction))
+            energy[position] = 1
+
+            direction = update_direction(position, direction)
+            if not direction:
+                return
+
+            position = update_position(position, direction)
+            if not in_bound(position):
+                return
+
+    run_light(pos, dir)
+    return int(energy.sum())
+
+print(re_run_light((0, 0), RIGHT))
+
+most_energy = 0
+most_energy = max(most_energy, max([re_run_light((i, 0), RIGHT) for i in range(max_i)]))
+most_energy = max(most_energy, max([re_run_light((max_i - 1, 0), LEFT) for i in range(max_i)]))
+most_energy = max(most_energy, max([re_run_light((0, j), DOWN) for j in range(max_j)]))
+most_energy = max(most_energy, max([re_run_light((0, max_j - 1), UP) for i in range(max_i)]))
+print(most_energy)
